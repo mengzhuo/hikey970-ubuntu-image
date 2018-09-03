e29a58602592ee7559e9503b3e9462d7e75d2e9c
Meng zhuo <mengzhuo1203@gmail.com>
2018-09-03 13:48:46 +0000

    remove all android net

--- rootfs/root/init.sh
+++ rootfs/root/init.sh
@@ -19,12 +19,5 @@ rm -f /etc/ssh/ssh_host_*
 echo "hikey970" > /etc/hostname
 echo "127.0.0.1 hikey970" >> /etc/hosts
 
-cat >> /etc/group << EOF
-aid_inet:x:3003:root,systemd-resolve,_apt,systemd-network,systemd-timesync     
-aid_net_raw:x:3004:root,systemd-resolve,_apt,systemd-network,systemd-timesync  
-aid_net_admin:x:3005:root,systemd-resolve,_apt,systemd-network,systemd-timesync
-EOF
-
-
 echo "self destroy, bye bye"
 rm /root/init.sh
